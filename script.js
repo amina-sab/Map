9589a63748991c5bf418b6ccf887e2af3e17f7aa
asabrina messikh <sab6401@gmail.com>
2022-04-11 19:46:31 +0200

    2

--- script.js
+++ script.js
@@ -3,6 +3,54 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    //this.date=...
+    //this.id=...
+    this.coords = coords; //[lat,lng]
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+class Running extends Workout {
+  type='running';
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    //min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  type='cycling';
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevation = elevationGain;
+    //this.type='cycling'
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    //km/h
+    this.speed = this.distance / this.duration / 60;
+    return this.speed;
+  }
+}
+
+// const run1=new Running([39,-12],5.2,24,178);
+// const cycle1=new Cycling([39,-12],27,95,523);
+// console.log(run1,cycle1);
+
+///////////////////////////////////////////
+//APPLICATION ARCHITECTURE
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -11,90 +59,131 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map,mapEvent;
-if (navigator.geolocation)
-  //pour recuper la position courante
-  navigator.geolocation.getCurrentPosition(
-    //premier calllback (succes)
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},@${longitude}`);
+class App {
+  #map;
+  #mapEvent;
+  #workouts = [];
 
-      const coords = [latitude, longitude];
-      //j'ai utilise une librairie leaflet, j'ai copié le script heberge en ligne par quelqu'un dans le .html
-      //dans la libraire open source leaflet il ya ce bloc de script pour ajouter un marquer et du texte sur la position courante
-       map = L.map('map').setView(coords, 13);
-      //console.log(map);
+  constructor() {
+    this._getPostion();
 
-      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-    //   L.marker(coords)
-    //     .addTo(map)
-    //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    //     .openPopup();
+    form.addEventListener('submit', this._newWorkout.bind(this));
 
-      //gerer les clicks sur la map
+    inputType.addEventListener('change', this._toggleElevationField);
+  }
 
-      map.on('click', function(mapE){
-          mapEvent=mapE;
-      form.classList.remove('hidden');
-      inputDistance.focus();
-
-
-
-//afficher le marquer
-       // console.log(mapEvent);
-// const {lat, lng}=mapEvent.latlng;
-// L.marker([lat,lng])
-//         .addTo(map)
-//         .bindPopup(L.popup({
-//             maxWidth:250,
-//             minWidth:100,
-//             autoClose:false,//pour ne pas fermer le popup
-//             closeOnClick:false,
-//             className:'running-popup',
-            
-//         }
-//         ))
-//         .setPopupContent('Workout')
-//         .openPopup();
-      }
-      );
-
-
-    }, //dexieme callback echec (erreur)
-    function () {
-      alert('could not get tour position');
-    }
-  );
-form.addEventListener('submit',function(e){
-
-e.preventDefault();
- 
-//supprimer lees entrees remplies
-inputDistance.value=inputDuration.value=inputCadence.value=inputElevation.value='';
-
-//afficher le marquer
-const {lat, lng}=mapEvent.latlng;
-L.marker([lat,lng])
-        .addTo(map)
-        .bindPopup(L.popup({
-            maxWidth:250,
-            minWidth:100,
-            autoClose:false,//pour ne pas fermer le popup
-            closeOnClick:false,
-            className:'running-popup',
-            
+  _getPostion() {
+    if (navigator.geolocation)
+      //pour recuper la position courante
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        function () {
+          alert('could not get tour position');
         }
-        ))
-        .setPopupContent('Workout')
-        .openPopup();
-});
-inputType.addEventListener('change',function(){
+      );
+  }
+
+  _loadMap(position) {
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.com/maps/@${latitude},@${longitude}`);
+
+    const coords = [latitude, longitude];
+    //j'ai utilise une librairie leaflet, j'ai copié le script heberge en ligne par quelqu'un dans le .html
+    //dans la libraire open source leaflet il ya ce bloc de script pour ajouter un marquer et du texte sur la position courante
+    this.#map = L.map('map').setView(coords, 13);
+    //console.log(map);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
 
-});
+  _newWorkout(e) {
+    e.preventDefault();
+
+    const validInputs = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+    //get data from form
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
+
+    //if activity runing , create runing object
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      //check if data is valid
+      if (
+        //!Number.isFinite(distance)||
+        // !Number.isFinite(duration)||
+        // !Number.isFinite(cadence)
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs have to be numbers!');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    //if activity rcycling , create cycling object
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be numbers!');
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    //add new object to workout array
+    this.#workouts.push(workout);
+    //render workout on map as marker
+    this.renderWorkoutMarker(workout);
+    //render workout on list
+    
+    //cacher le form et supprimer lees entrees remplies
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        '';
+  }
+  renderWorkoutMarker(workout) {
+    L.marker(workout.coords)
+      .addTo(this.#map)
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false, //pour ne pas fermer le popup
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      )
+      .setPopupContent('workout')
+      .openPopup();
+  }
+}
+
+const app = new App();
